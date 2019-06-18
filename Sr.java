0ca032e9b18f709aadeb19bb503efbbac09c9b15
Shirisha Reddy Dhanasiri <sdhanas1@in.ibm.com>
2019-06-18 16:50:39 +0530

    new branch

---
+++ Sr.java
@@ -0,0 +1,5 @@
+class Sr
+public static void main(String args[]){
+System.out.println("hi");
+}
+}
--- Tr1.java
+++
@@ -1,5 +0,0 @@
-class Tr{
-public static void main(String args[]){
-System.out.println("waaste tr");
-}
-}
